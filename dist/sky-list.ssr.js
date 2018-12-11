bbd271b35650086c05d4ae6fa1c0ef7b06042fe4
Rudi Ørnhøj <rornhoj@gmail.com>
2018-12-11 09:33:00 +0100

    Scraping list setup

--- dist/sky-list.ssr.js
+++ dist/sky-list.ssr.js
@@ -6,14 +6,12 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 
 var axios = _interopDefault(require('axios'));
 var qs = _interopDefault(require('qs'));
-var debounce = _interopDefault(require('debounce'));
+
+// import debounce from 'debounce';
 
 var defaultOptions = {
 	api: '/umbraco/api/site/search/',
 	limit: 10,
-	showCount: false,
-	filterType: 'request',
-	paginationType: 'more', // navigation | pagination | more | all | numeric
 	immediate: false,
 };
 
@@ -22,6 +20,7 @@ function getQueryParams() {
 		var q = window.location.search.replace('?', '');
 		return qs.parse(q);
 	}
+
 	return {};
 }
 
@@ -39,21 +38,8 @@ function setQueryParams(params, skipNulls) {
 var script = {
 	name: 'SkyList',
 	props: {
-		// override query with external values
-		query: {
-			type: Object,
-			default: function () { return ({}); },
-		},
-		// Value map is used for fetching vue-multiselect value
-		valueMap: {
-			type: Object,
-			default: function () { return ({}); },
-		},
-		filter: {
-			type: Object,
-			default: function () { return ({}); },
-		},
-		initialValues: {
+		// Set up parameters to v-model
+		parameters: {
 			type: Object,
 			default: function () { return ({}); },
 		},
@@ -82,11 +68,9 @@ var script = {
 	data: function data() {
 		return {
 			previousQuery: {},
-			listQuery: Object.assign(
+			query: Object.assign(
 				{},
-				this.filter,
-				this.initialValues,
-				this.query,
+				this.parameters,
 				getQueryParams() // initiate with query params from url
 			),
 			config: Object.assign(
@@ -99,13 +83,9 @@ var script = {
 				cancelToken: null,
 				loading: false,
 			},
-			pages: {
-				max: 1,
-				current: 1,
-			},
 			result: {
 				data: [],
-				groups: [],
+				// groups: [],
 				pagination: {
 					limit: this.options.limit || defaultOptions.limit,
 					offset: 0,
@@ -115,27 +95,6 @@ var script = {
 		};
 	},
 	computed: {
-		numericPagination: function numericPagination() {
-			return (this.config.paginationType === 'numeric') || (this.config.paginationType === 'pagination');
-		},
-		flowPagination: function flowPagination() {
-			return (this.config.paginationType === 'navigation') || (this.config.paginationType === 'pagination');
-		},
-		morePagination: function morePagination() {
-			return (this.config.paginationType === 'more') || (this.config.paginationType === 'all');
-		},
-		showPagination: function showPagination() {
-			if (!this.states.hasFetchedOnce) {
-				return false;
-			}
-			if (this.morePagination) {
-				return this.canFetchMore;
-			}
-			return this.pages.max > 1;
-		},
-		filterKeys: function filterKeys() {
-			return Object.keys(this.filter);
-		},
 		itemsLeft: function itemsLeft() {
 			// Provides the amount of items not displayed yet.
 			var ref = this.result.pagination;
@@ -147,31 +106,27 @@ var script = {
 				? total - (offset + limit)
 				: limit;
 		},
-		resultStartIndex: function resultStartIndex() {
-			if (this.morePagination) {
-				return 0;
-			}
+		// resultStartIndex() {
+		// 	if (this.morePagination) {
+		// 		return 0;
+		// 	}
 
-			return (this.pages.current - 1) * this.config.limit;
-		},
-		resultEndIndex: function resultEndIndex() {
-			var ref = this.result;
-			var ref_pagination = ref.pagination;
-			var offset = ref_pagination.offset;
-			var limit = ref_pagination.limit;
-			var total = ref_pagination.total;
-			if (total === null) {
-				return offset + limit;
-			}
+		// 	return (this.pages.current - 1) * this.config.limit;
+		// },
+		// resultEndIndex() {
+		// 	const { pagination: { offset, limit, total } } = this.result;
+		// 	if (total === null) {
+		// 		return offset + limit;
+		// 	}
 
-			return Math.min(offset + limit, total);
-		},
+		// 	return Math.min(offset + limit, total);
+		// },
 		currentResultSet: function currentResultSet() {
 			// Showing the part of the complete resultset currently wanted.
 			var ref = this.result;
 			var data = ref.data;
 
-			return data[this.pageNoToIndex(this.getCurrentPage())] || [];
+			return data;
 		},
 		limitEnd: function limitEnd() {
 			return this.result.pagination.offset + this.result.pagination.limit;
@@ -186,9 +141,9 @@ var script = {
 			var res = {};
 			var extractObjectKey = Object.keys(this.valueMap);
 
-			Object.keys(this.listQuery).forEach(function (key) {
-				if (Array.isArray(this$1.listQuery[key])) {
-					res[key] = this$1.listQuery[key]
+			Object.keys(this.query).forEach(function (key) {
+				if (Array.isArray(this$1.query[key])) {
+					res[key] = this$1.query[key]
 						.map(function (queryProperty) {
 							// if queryProperty contains a 'value' property return that
 							if (queryProperty && typeof queryProperty.value !== 'undefined') {
@@ -203,15 +158,15 @@ var script = {
 							 * and use exposed value mapping for grapping object value.
 							 */
 					res[key] = !extractObjectKey.includes(key)
-						? this$1.listQuery[key]
-						: this$1.listQuery[key][this$1.valueMap[key]];
+						? this$1.query[key]
+						: this$1.query[key][this$1.valueMap[key]];
 				}
 			});
 			return res;
 		},
 		validQuery: function validQuery() {
 			return (typeof this.validateQuery === 'function')
-				? this.validateQuery(this.listQuery)
+				? this.validateQuery(this.query)
 				: this.validateQuery;
 		},
 		requestParams: function requestParams() {
@@ -223,39 +178,32 @@ var script = {
 			return rp;
 		},
 	},
-	watch: {
-		query: {
-			handler: function handler() {
-				this.states.loading = true;
-				this.updateListQuery();
-			},
-			deep: true,
-		},
-		listQuery: {
-			handler: function handler() {
-				if (this.liveSearch && this.validQuery) {
-					this.handleUserSearch();
-				} else if (!this.validQuery) {
-					// Clear request params from url
-					this.updateUrlParams({});
-				}
-			},
-			deep: true,
-		},
-		'states.loading': function(value) {
-			if (value) {
-				this.$emit('loadingBegin');
-			} else {
-				this.$emit('loadingEnd');
-			}
-		},
-		result: {
-			handler: function handler(value) {
-				this.$emit('result', value);
-			},
-			deep: true,
-		},
-	},
+	// watch: {
+	// 	query: {
+	// 		handler() {
+	// 			if (this.liveSearch && this.validQuery) {
+	// 				this.handleUserSearch();
+	// 			} else if (!this.validQuery) {
+	// 				// Clear request params from url
+	// 				this.updateUrlParams({});
+	// 			}
+	// 		},
+	// 		deep: true,
+	// 	},
+	// 	'states.loading': function(value) {
+	// 		if (value) {
+	// 			this.$emit('loadingBegin');
+	// 		} else {
+	// 			this.$emit('loadingEnd');
+	// 		}
+	// 	},
+	// 	result: {
+	// 		handler(value) {
+	// 			this.$emit('result', value);
+	// 		},
+	// 		deep: true,
+	// 	},
+	// },
 	mounted: function mounted() {
 		// Do fetch on mount, if configured to or if initiated with valid query from url params
 		if (this.config.immediate || this.validQuery) {
@@ -263,117 +211,12 @@ var script = {
 		}
 	},
 	methods: {
-		updateListQuery: debounce(function() {
-			this.$set(this, 'listQuery', Object.assign({}, this.filter, this.initialValues, this.query));
-		}, 200),
-		// This method runs on liveSearch = true, so we make sure to debounce it by 100ms
-		// so we don't spam the server needlessly
-		handleUserSearch: function handleUserSearch() {
-			var this$1 = this;
-
-			if (this.validQuery) {
-				Object.keys(this.listQuery).forEach(function (key) {
-					var changedKey = this$1.listQuery[key] !== this$1.previousQuery[key];
-					var isFilterKey = this$1.filterKeys.includes(key);
-
-					if (changedKey && isFilterKey) {
-						this$1.filterData();
-					} else if (changedKey) {
-						this$1.request();
-					}
-
-					if (changedKey) {
-						Object.assign(this$1.previousQuery, this$1.listQuery);
-					}
-				});
-			} else {
-				this.resetPagination();
-			}
-		},
-		nativeSearchHandling: function nativeSearchHandling() {
-			this.validQuery
-				// Handle pressing enter
-				? this.request()
-				// Handle clearing input
-				: this.resetPagination();
-		},
-		setCurrentPage: function setCurrentPage(pageNo) {
-			this.pages.current = pageNo;
-		},
-		getCurrentPage: function getCurrentPage() {
-			return this.pages.current;
-		},
-		pageNoToIndex: function pageNoToIndex(value) {
-			return value ? --value : 0;
-		},
-		requestedPageIsFetched: function requestedPageIsFetched(pageNoRequested) {
-			var ref = this.result;
-			var data = ref.data;
-
-			return data[pageNoRequested - 1];
-		},
-		filterData: function filterData() {
-			var this$1 = this;
-
-			var ref = this.result;
-			var groups = ref.groups;
-			var total = ref.pagination.total;
-
-			var currentArea = groups
-				? groups.find(function (area) { return area.id === this$1.listQuery.area; })
-				: null;
-			var currentAreaCount = (currentArea && currentArea.count)
-				? currentArea.count
-				: total;
-
-			this.setCurrentPage(1);
-			this.setPagination({ offset: 0, total: currentAreaCount });
-
-			this.request('filter');
-		},
 		more: function more(all) {
-			var ref = this.result.pagination;
-			var limit = ref.limit;
-			var total = ref.total;
-			var offset = ref.offset;
-			var newPagination = { offset: offset + limit };
 
-			if (all) {
-				newPagination.limit = total - offset;
-			}
-
-			this.setPagination(newPagination);
-
-			this.request('append');
 		},
-		goTo: function goTo(target) {
-			var ref = this.result.pagination;
-			var limit = ref.limit;
-			var offset = ref.offset;
-
-			if (target === 'next') {
-				this.setCurrentPage(Math.min(this.getCurrentPage() + 1, this.pages.max));
-				this.setPagination({ offset: offset + limit });
-			}
-
-			if (target === 'previous') {
-				this.setCurrentPage(Math.min(this.getCurrentPage() - 1, this.pages.max));
-				this.setPagination({ offset: Math.max(offset - limit, 0) });
-			}
-
-			if (Number(target)) {
-				this.setCurrentPage(target);
-				this.setPagination({ offset: (this.getCurrentPage() - 1) * limit });
-			}
-
-			if (!this.requestedPageIsFetched(this.getCurrentPage())) {
-				this.request('page', this.getCurrentPage());
-			}
-		},
-		request: function request(type, pageNo) {
+		request: function request(type) {
 			var this$1 = this;
 			if ( type === void 0 ) type = 'clean';
-			if ( pageNo === void 0 ) pageNo = 1;
 
 			this.states.loading = true;
 			var ref = this.result.pagination;
@@ -392,10 +235,10 @@ var script = {
 							limit: this$1.limitEnd, // hvorfor limit = limitEnd?
 							offset: 0,
 						})).then(function (secondaryResult) {
-							this$1.dataParser(secondaryResult, type, pageNo);
+							this$1.$set(this$1.result, 'data', result.data);
 						});
 					} else {
-						this$1.dataParser(result, type, pageNo);
+						this$1.$set(this$1.result, 'data', result.data);
 					}
 				})
 				.then(function () {
@@ -411,52 +254,6 @@ var script = {
 				this.states.loading = false;
 			}
 			// TODO: handle error?⁄
-		},
-		dataParser: function dataParser(result, type, pageNo) {
-			var this$1 = this;
-			if ( type === void 0 ) type = 'clean';
-
-			var data = result.data;
-			var pagination = result.pagination;
-			var areas = result.areas;
-
-			this.pages.max = Math.ceil(pagination.total / pagination.limit);
-
-			var dataActions = {
-				append: function () {
-					// Append fetched result to existing.
-					this$1.setResultData(this$1.result.data[0].concat( data));
-				},
-				page: function () {
-					this$1.setResultData(data, this$1.pageNoToIndex(pageNo));
-				},
-				clean: function () {
-					// Reset result object to newly fetched result.
-					this$1.setResultData(data, this$1.pageNoToIndex(pageNo));
-					this$1.setPagination({ offset: 0, total: pagination.total });
-					this$1.setGroups(areas);
-				},
-				filter: function () {
-					// Resets all, but group info and query
-					this$1.deleteResultData();
-
-					this$1.setResultData(data, this$1.pageNoToIndex(pageNo));
-				},
-			};
-
-			dataActions[type]();
-
-			// add any unknown custom result props to result object, since we only
-			// handle data, pagination and areas above
-			Object.keys(result)
-				.filter(function (key) {
-					return key !== 'data' && key !== 'pagination' && key !== 'areas';
-				})
-				.forEach(function (key) {
-					this$1.result[key] = result[key];
-				});
-
-			this.states.loading = false;
 		},
 		fetch: function fetch(params) {
 			var this$1 = this;
@@ -491,39 +288,6 @@ var script = {
 		updateUrlParams: function updateUrlParams(params) {
 			setQueryParams(params);
 		},
-		resetQuery: function resetQuery() {
-			var this$1 = this;
-
-			this.filterKeys.forEach(function (param) {
-				this$1.listQuery[param] = this$1.filter[param];
-			});
-
-			this.resetPagination();
-		},
-		resetPagination: function resetPagination() {
-			this.setPagination({ offset: 0, total: 0 });
-		},
-		setGroups: function setGroups(groups) {
-			this.$set(this.result, 'groups', groups);
-		},
-		setPagination: function setPagination(pagination) {
-			this.$set(this.result, 'pagination', Object.assign(this.result.pagination, pagination));
-		},
-		setResultData: function setResultData(data, index) {
-			var this$1 = this;
-			if ( index === void 0 ) index = 0;
-
-			for (var i = 0; i < index; i++) {
-				if (!this$1.result.data[i]) {
-					this$1.result.data.splice(i, 1, null);
-				}
-			}
-
-			this.result.data.splice(index, 1, data);
-		},
-		deleteResultData: function deleteResultData() {
-			this.result.data.splice(0);
-		},
 	},
 };
 
@@ -538,7 +302,7 @@ var __vue_staticRenderFns__ = [];
   /* scoped */
   var __vue_scope_id__ = undefined;
   /* module identifier */
-  var __vue_module_identifier__ = "data-v-ed180d66";
+  var __vue_module_identifier__ = "data-v-42393e22";
   /* functional template */
   var __vue_is_functional_template__ = false;
   /* component normalizer */
--- src/SkyList.js
+++ src/SkyList.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 import qs from 'qs';
-import debounce from 'debounce';
+// import debounce from 'debounce';
 
 const defaultOptions = {
 	api: '/umbraco/api/site/search/',
 	limit: 10,
-	showCount: false,
-	filterType: 'request',
-	paginationType: 'more', // navigation | pagination | more | all | numeric
 	immediate: false,
 };
 
@@ -16,6 +13,7 @@ function getQueryParams() {
 		const q = window.location.search.replace('?', '');
 		return qs.parse(q);
 	}
+
 	return {};
 }
 
@@ -31,21 +29,8 @@ function setQueryParams(params, skipNulls = true) {
 export default {
 	name: 'SkyList',
 	props: {
-		// override query with external values
-		query: {
-			type: Object,
-			default: () => ({}),
-		},
-		// Value map is used for fetching vue-multiselect value
-		valueMap: {
-			type: Object,
-			default: () => ({}),
-		},
-		filter: {
-			type: Object,
-			default: () => ({}),
-		},
-		initialValues: {
+		// Set up parameters to v-model
+		parameters: {
 			type: Object,
 			default: () => ({}),
 		},
@@ -74,11 +59,9 @@ export default {
 	data() {
 		return {
 			previousQuery: {},
-			listQuery: Object.assign(
+			query: Object.assign(
 				{},
-				this.filter,
-				this.initialValues,
-				this.query,
+				this.parameters,
 				getQueryParams(), // initiate with query params from url
 			),
 			config: Object.assign(
@@ -91,13 +74,9 @@ export default {
 				cancelToken: null,
 				loading: false,
 			},
-			pages: {
-				max: 1,
-				current: 1,
-			},
 			result: {
 				data: [],
-				groups: [],
+				// groups: [],
 				pagination: {
 					limit: this.options.limit || defaultOptions.limit,
 					offset: 0,
@@ -107,27 +86,6 @@ export default {
 		};
 	},
 	computed: {
-		numericPagination() {
-			return (this.config.paginationType === 'numeric') || (this.config.paginationType === 'pagination');
-		},
-		flowPagination() {
-			return (this.config.paginationType === 'navigation') || (this.config.paginationType === 'pagination');
-		},
-		morePagination() {
-			return (this.config.paginationType === 'more') || (this.config.paginationType === 'all');
-		},
-		showPagination() {
-			if (!this.states.hasFetchedOnce) {
-				return false;
-			}
-			if (this.morePagination) {
-				return this.canFetchMore;
-			}
-			return this.pages.max > 1;
-		},
-		filterKeys() {
-			return Object.keys(this.filter);
-		},
 		itemsLeft() {
 			// Provides the amount of items not displayed yet.
 			const { limit, offset, total } = this.result.pagination;
@@ -136,26 +94,26 @@ export default {
 				? total - (offset + limit)
 				: limit;
 		},
-		resultStartIndex() {
-			if (this.morePagination) {
-				return 0;
-			}
+		// resultStartIndex() {
+		// 	if (this.morePagination) {
+		// 		return 0;
+		// 	}
 
-			return (this.pages.current - 1) * this.config.limit;
-		},
-		resultEndIndex() {
-			const { pagination: { offset, limit, total } } = this.result;
-			if (total === null) {
-				return offset + limit;
-			}
+		// 	return (this.pages.current - 1) * this.config.limit;
+		// },
+		// resultEndIndex() {
+		// 	const { pagination: { offset, limit, total } } = this.result;
+		// 	if (total === null) {
+		// 		return offset + limit;
+		// 	}
 
-			return Math.min(offset + limit, total);
-		},
+		// 	return Math.min(offset + limit, total);
+		// },
 		currentResultSet() {
 			// Showing the part of the complete resultset currently wanted.
 			const { data } = this.result;
 
-			return data[this.pageNoToIndex(this.getCurrentPage())] || [];
+			return data;
 		},
 		limitEnd() {
 			return this.result.pagination.offset + this.result.pagination.limit;
@@ -168,9 +126,9 @@ export default {
 			const res = {};
 			const extractObjectKey = Object.keys(this.valueMap);
 
-			Object.keys(this.listQuery).forEach((key) => {
-				if (Array.isArray(this.listQuery[key])) {
-					res[key] = this.listQuery[key]
+			Object.keys(this.query).forEach((key) => {
+				if (Array.isArray(this.query[key])) {
+					res[key] = this.query[key]
 						.map((queryProperty) => {
 							// if queryProperty contains a 'value' property return that
 							if (queryProperty && typeof queryProperty.value !== 'undefined') {
@@ -185,15 +143,15 @@ export default {
 							 * and use exposed value mapping for grapping object value.
 							 */
 					res[key] = !extractObjectKey.includes(key)
-						? this.listQuery[key]
-						: this.listQuery[key][this.valueMap[key]];
+						? this.query[key]
+						: this.query[key][this.valueMap[key]];
 				}
 			});
 			return res;
 		},
 		validQuery() {
 			return (typeof this.validateQuery === 'function')
-				? this.validateQuery(this.listQuery)
+				? this.validateQuery(this.query)
 				: this.validateQuery;
 		},
 		requestParams() {
@@ -205,39 +163,32 @@ export default {
 			return rp;
 		},
 	},
-	watch: {
-		query: {
-			handler() {
-				this.states.loading = true;
-				this.updateListQuery();
-			},
-			deep: true,
-		},
-		listQuery: {
-			handler() {
-				if (this.liveSearch && this.validQuery) {
-					this.handleUserSearch();
-				} else if (!this.validQuery) {
-					// Clear request params from url
-					this.updateUrlParams({});
-				}
-			},
-			deep: true,
-		},
-		'states.loading': function(value) {
-			if (value) {
-				this.$emit('loadingBegin');
-			} else {
-				this.$emit('loadingEnd');
-			}
-		},
-		result: {
-			handler(value) {
-				this.$emit('result', value);
-			},
-			deep: true,
-		},
-	},
+	// watch: {
+	// 	query: {
+	// 		handler() {
+	// 			if (this.liveSearch && this.validQuery) {
+	// 				this.handleUserSearch();
+	// 			} else if (!this.validQuery) {
+	// 				// Clear request params from url
+	// 				this.updateUrlParams({});
+	// 			}
+	// 		},
+	// 		deep: true,
+	// 	},
+	// 	'states.loading': function(value) {
+	// 		if (value) {
+	// 			this.$emit('loadingBegin');
+	// 		} else {
+	// 			this.$emit('loadingEnd');
+	// 		}
+	// 	},
+	// 	result: {
+	// 		handler(value) {
+	// 			this.$emit('result', value);
+	// 		},
+	// 		deep: true,
+	// 	},
+	// },
 	mounted() {
 		// Do fetch on mount, if configured to or if initiated with valid query from url params
 		if (this.config.immediate || this.validQuery) {
@@ -245,102 +196,10 @@ export default {
 		}
 	},
 	methods: {
-		updateListQuery: debounce(function() {
-			this.$set(this, 'listQuery', Object.assign({}, this.filter, this.initialValues, this.query));
-		}, 200),
-		// This method runs on liveSearch = true, so we make sure to debounce it by 100ms
-		// so we don't spam the server needlessly
-		handleUserSearch() {
-			if (this.validQuery) {
-				Object.keys(this.listQuery).forEach((key) => {
-					const changedKey = this.listQuery[key] !== this.previousQuery[key];
-					const isFilterKey = this.filterKeys.includes(key);
-
-					if (changedKey && isFilterKey) {
-						this.filterData();
-					} else if (changedKey) {
-						this.request();
-					}
-
-					if (changedKey) {
-						Object.assign(this.previousQuery, this.listQuery);
-					}
-				});
-			} else {
-				this.resetPagination();
-			}
-		},
-		nativeSearchHandling() {
-			this.validQuery
-				// Handle pressing enter
-				? this.request()
-				// Handle clearing input
-				: this.resetPagination();
-		},
-		setCurrentPage(pageNo) {
-			this.pages.current = pageNo;
-		},
-		getCurrentPage() {
-			return this.pages.current;
-		},
-		pageNoToIndex(value) {
-			return value ? --value : 0;
-		},
-		requestedPageIsFetched(pageNoRequested) {
-			const { data } = this.result;
-
-			return data[pageNoRequested - 1];
-		},
-		filterData() {
-			const { groups, pagination: { total } } = this.result;
-
-			const currentArea = groups
-				? groups.find(area => area.id === this.listQuery.area)
-				: null;
-			const currentAreaCount = (currentArea && currentArea.count)
-				? currentArea.count
-				: total;
-
-			this.setCurrentPage(1);
-			this.setPagination({ offset: 0, total: currentAreaCount });
-
-			this.request('filter');
-		},
 		more(all) {
-			const { limit, total, offset } = this.result.pagination;
-			const newPagination = { offset: offset + limit };
 
-			if (all) {
-				newPagination.limit = total - offset;
-			}
-
-			this.setPagination(newPagination);
-
-			this.request('append');
 		},
-		goTo(target) {
-			const { limit, offset } = this.result.pagination;
-
-			if (target === 'next') {
-				this.setCurrentPage(Math.min(this.getCurrentPage() + 1, this.pages.max));
-				this.setPagination({ offset: offset + limit });
-			}
-
-			if (target === 'previous') {
-				this.setCurrentPage(Math.min(this.getCurrentPage() - 1, this.pages.max));
-				this.setPagination({ offset: Math.max(offset - limit, 0) });
-			}
-
-			if (Number(target)) {
-				this.setCurrentPage(target);
-				this.setPagination({ offset: (this.getCurrentPage() - 1) * limit });
-			}
-
-			if (!this.requestedPageIsFetched(this.getCurrentPage())) {
-				this.request('page', this.getCurrentPage());
-			}
-		},
-		request(type = 'clean', pageNo = 1) {
+		request(type = 'clean') {
 			this.states.loading = true;
 			const { total, offset } = this.result.pagination;
 
@@ -356,10 +215,10 @@ export default {
 							limit: this.limitEnd, // hvorfor limit = limitEnd?
 							offset: 0,
 						})).then((secondaryResult) => {
-							this.dataParser(secondaryResult, type, pageNo);
+							this.$set(this.result, 'data', result.data);
 						});
 					} else {
-						this.dataParser(result, type, pageNo);
+						this.$set(this.result, 'data', result.data);
 					}
 				})
 				.then(() => {
@@ -375,47 +234,6 @@ export default {
 				this.states.loading = false;
 			}
 			// TODO: handle error?⁄
-		},
-		dataParser(result, type = 'clean', pageNo) {
-			const { data, pagination, areas } = result;
-
-			this.pages.max = Math.ceil(pagination.total / pagination.limit);
-
-			const dataActions = {
-				append: () => {
-					// Append fetched result to existing.
-					this.setResultData([...this.result.data[0], ...data]);
-				},
-				page: () => {
-					this.setResultData(data, this.pageNoToIndex(pageNo));
-				},
-				clean: () => {
-					// Reset result object to newly fetched result.
-					this.setResultData(data, this.pageNoToIndex(pageNo));
-					this.setPagination({ offset: 0, total: pagination.total });
-					this.setGroups(areas);
-				},
-				filter: () => {
-					// Resets all, but group info and query
-					this.deleteResultData();
-
-					this.setResultData(data, this.pageNoToIndex(pageNo));
-				},
-			};
-
-			dataActions[type]();
-
-			// add any unknown custom result props to result object, since we only
-			// handle data, pagination and areas above
-			Object.keys(result)
-				.filter((key) => {
-					return key !== 'data' && key !== 'pagination' && key !== 'areas';
-				})
-				.forEach((key) => {
-					this.result[key] = result[key];
-				});
-
-			this.states.loading = false;
 		},
 		fetch(params = this.requestParams) {
 			// Cancel previous request
@@ -446,34 +264,6 @@ export default {
 		},
 		updateUrlParams(params) {
 			setQueryParams(params);
-		},
-		resetQuery() {
-			this.filterKeys.forEach((param) => {
-				this.listQuery[param] = this.filter[param];
-			});
-
-			this.resetPagination();
-		},
-		resetPagination() {
-			this.setPagination({ offset: 0, total: 0 });
-		},
-		setGroups(groups) {
-			this.$set(this.result, 'groups', groups);
-		},
-		setPagination(pagination) {
-			this.$set(this.result, 'pagination', Object.assign(this.result.pagination, pagination));
-		},
-		setResultData(data, index = 0) {
-			for (let i = 0; i < index; i++) {
-				if (!this.result.data[i]) {
-					this.result.data.splice(i, 1, null);
-				}
-			}
-
-			this.result.data.splice(index, 1, data);
-		},
-		deleteResultData() {
-			this.result.data.splice(0);
 		},
 	},
 };
